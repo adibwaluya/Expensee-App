55c58994153db89059d976545355aaef6eb237c9
s0567271 <adib.waluya@student.htw-berlin.de>
2020-11-12 13:44:38 +0100

    Created fragments for mainpage and userlogin

---
+++ Expensee/app/src/main/java/de/htwberlin/expensee/screen/login/UserLoginFragment.kt
@@ -0,0 +1,38 @@
+package de.htwberlin.expensee.screen.login
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import de.htwberlin.expensee.R
+import de.htwberlin.expensee.databinding.FragmentLoginBinding
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [UserLoginFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class UserLoginFragment : Fragment() {
+
+    private lateinit var binding: FragmentLoginBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_login,
+            container,
+            false
+        )
+        return binding.root
+    }
+
+}
