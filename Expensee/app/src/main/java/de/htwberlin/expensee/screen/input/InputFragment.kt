6787b5c588b19fbf726f4379e7fdba6bc445f507
Michael Reno <michaelreno19@gmail.com>
2020-11-16 21:14:32 +0100

    Set onClickListener to change input color based on its sign

--- Expensee/app/src/main/java/de/htwberlin/expensee/screen/input/InputFragment.kt
+++ Expensee/app/src/main/java/de/htwberlin/expensee/screen/input/InputFragment.kt
@@ -1,5 +1,6 @@
 package de.htwberlin.expensee.screen.input
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -16,12 +17,25 @@ class InputFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        var vorzeichenClick = 0
+
         binding = DataBindingUtil.inflate(
             inflater,
             R.layout.fragment_input,
             container,
             false
         )
+
+        binding.vorzeichenButton.setOnClickListener {  // Change the color of input whether it is positive or negative
+            var input = binding.transactionInput
+            if (vorzeichenClick % 2 == 0) {
+                input.setTextColor(Color.GREEN)
+            }
+            else {
+                input.setTextColor(Color.RED)
+            }
+        }
+
         return binding.root
     }
 }
